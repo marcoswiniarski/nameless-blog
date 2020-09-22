053515dba83705a1ac24b1d0a1f8f19325bbfbd2
Marcos Eduardo Winiarski <marcos.winiarski@hbsis.com.br>
2020-09-21 21:41:34 -0300

    Ajuste config do gatsby

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `nameless blog`,
+    description: `Um blog sem nome para divagar sobre tecnologia e coisas legais.`,
+    author: `nameless`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
