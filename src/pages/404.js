c5075d232d548a5f92786e766d7d2a747ee055e2
Marcos Eduardo Winiarski <marcos.winiarski@hbsis.com.br>
2020-09-21 22:47:09 -0300

    Criação de componente de perfil para o blog

---
+++ src/pages/404.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Erro = () => (
+  <h1>Erro</h1>
+)
+
+export default Erro
---
+++ src/pages/sobre.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import Layout from '../components/layout'
+import SEO from '../components/seo'
+
+const Sobre = () => (
+  <Layout>
+    <SEO title='Sobre' />
+    <h1>Sobre</h1>
+  </Layout>
+)
+
+export default Sobre
