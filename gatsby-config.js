ab40fd6440eebf4927d9a3c9fa1f32f93abc28bf
Marcos Eduardo Winiarski <marcos.winiarski@hbsis.com.br>
2020-09-24 22:04:49 -0300

    Adicão do styled-components no projeto

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,7 @@ module.exports = {
     author: `nameless`,
   },
   plugins: [
+    `gatsby-plugin-styled-components`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
