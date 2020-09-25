4e8215e1b284b98eea184d353d5f0a28a9f0ed3c
Marcos Eduardo Winiarski <marcos.winiarski@hbsis.com.br>
2020-09-24 23:07:02 -0300

    Criação de componente de sidebar da app

---
+++ src/components/Sidebar/styled.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+export const SidebarWrapper = styled.aside`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  position: fixed;
+  width: 20rem;
+  height: 100vh;
+  padding: 2rem;
+  background: #192734;
+  border-right 1px solid #38444d;
+  text-align: center;
+`
